1fff0e4da42ccb65391490d30f20e05a29af2fc7
Cmperez238 <132858687+Cmperez238@users.noreply.github.com>
2023-05-07 20:13:40 -0500

    Plantilla de comienzo

--- Proyecto de programacion.cpp
+++ Proyecto de programacion.cpp
@@ -1,8 +1,35 @@
-#include <iostream>
+#include <iostream> 
+#include <vector>
+#include <sstring>
+
 using namespace std;
 
+struct tPolinomio;
+
+{ 
+	int grado;                /* Grado del polinomio */
+    vector<double> coef;      /* Coeficientes */
+};
+
 int main () {
-    cout << "prueba";
+
+    int n, j {0};
+    string equt;
+    stringstream ss;
+    cout << "Introduzca la cantidad de polinomios a sumar: ";
+    cin >> n;
+    cin.ignore();
+
+    for (i=1; i<=n; i++)
+        tPolinomio poli[i];
+
+    do{
+    j++;
+    cout << endl << "Polinomio " << j << " :";
+    getline (cin, equt);
+    ss << 
+
+    } (while j<=n);
 
     return 0;
 }
