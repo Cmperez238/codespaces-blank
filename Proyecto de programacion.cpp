f8f49fcf3890fca7fd62c38f72800932d842ae57
Cmperez238 <132858687+Cmperez238@users.noreply.github.com>
2023-05-09 18:49:51 -0500

    Update Proyecto de programacion.cpp

--- Proyecto de programacion.cpp
+++ Proyecto de programacion.cpp
@@ -8,7 +8,7 @@ using namespace std;
 struct tPolinomio
 { 
 	int grado;                /* Grado del polinomio */
-    vector<double> coef;      /* Coeficientes */
+    vector <double> coef;      /* Coeficientes */
 };
 
 int main () {
@@ -23,76 +23,83 @@ int main () {
     cin.ignore();
 
     for (i=1; i<=n; i++)
+    {
         tPolinomio poli[i];
 
-
-    do{
-        cout << endl << "Polinomio " << j << " :";
-        getline (cin, equt);
-        
-        for (auto C : equt) // Copia en C cada char de equt y va analizando si es un espacio (Usamos chatGTP para conocer de isspace)
-        {
-            if (not (isspace(C)))
+            cout << endl << "Polinomio " << i << " :";
+            getline (cin, equt);
+            
+            for (auto C : equt) // Copia en C cada char de equt y va analizando si es un espacio (Usamos chatGTP para conocer de isspace)
             {
-                vec.push_back(C);
+                if (not (isspace(C)))
+                {
+                    vec.push_back(C);
+                }
             }
-        }
 
-        string rawpol(vec.data(), vec.size());  // Este nuevo string simplemente abra ignorado todos los espacios que el usuario ha insertado
-        vec.clear();
-        int p{0};
+            string rawpol(vec.data(), vec.size());  // Este nuevo string simplemente abra ignorado todos los espacios que el usuario ha insertado
+            vec.clear();
+            int p{0};
 
-        for (auto C : rawpol)
-        {
-            p++;
-            if (not(p==1 and (C=='+')))
+            for (auto C : rawpol)
             {
-                vec.push_back(C);
+                p++;
+                if (not(p==1 and (C=='+')))
+                {
+                    vec.push_back(C);
+                }
             }
-        }
 
-        string pol(vec.data(), vec.size()); //Este nuevo string no contiene un mas al inicio en caso de que contenga uno
-        vec.clear();
+            string pol(vec.data(), vec.size()); //Este nuevo string no contiene un mas al inicio en caso de que contenga uno
+            vec.clear();
 
-        
-        vector <char> comp;    // Vector de componentes de cada monomio
-        vector <string> mono;  // Vector donde se almacenan los monomios
-        p=0;       //Solo tiene un solo uso
+            
+            // Vector de componentes de cada monomio, se almacenara en vec
+            vector <string> mono;  // Vector donde se almacenan los monomios
+            p=0;       //Solo tiene un solo uso
 
-        for (auto C : pol)
-        {
-            p++;
-            if (p==1)
+            for (auto C : pol)
             {
-                comp.push_back(C);
+                p++;
+                if (p==1)  // Hay dos opciones o bien es un entero o es -
+                {
+                    vec.push_back(C);
+                }
+                else if (p!=1 and (C=='-' or C=='+'))
+                {
+                    string mon(vec.data(), vec.size());                // En este else if se entrara solo si C es + ó -
+                    mono.push_back(mon);                                 // Una vez adentro se creara un string de cada monomio y se almacenara en el vector mono
+                    vec.clear();
+                    vec.push_back(C);
+                }
+                else if (p==size(pol))
+                {
+                    vec.push_back(C);
+                    string mon(vec.data(), vec.size());
+                    mono.push_back(mon);
+                    vec.clear();
+                }
+                else
+                {
+                    vec.push_back(C);
+                }
             }
-            else if (p!=1 and (C=='-' or C=='+'))
-            {
-                string mon(comp.data(), comp.size());                // En este else if se entrara solo si C es + ó -
-                mono.push_back(mon);                                 // Una vez adentro se creara un string de cada monomio y se almacenara en el vector mono
-                comp.clear();
-                comp.push_back(C);
-            }
-            else if (p==size(pol))
-            {
-                comp.push_back(C);
-                string mon(comp.data(), comp.size());
-                mono.push_back(mon);
-                comp.clear();
-            }
-            else
-            {
-                comp.push_back(C);
-            }
-        }
 
-        for (string m : mono)
-            cout << m << endl;
+            for (auto mon : mono)
+                ss << mon;
+                double num;
+                ss >> num;
+                poli[i].coef.push_back(num);
+                
+            
 
-        
+                
 
-        j++;
-    } while (j<=n);
+
+
+            
+
+    }
 
     return 0;
 }
