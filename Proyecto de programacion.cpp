750850c92590df4c162e8114379e33b3fff6d9d9
Cmperez238 <132858687+Cmperez238@users.noreply.github.com>
2023-05-08 22:21:38 -0500

    Update Proyecto de programacion.cpp
    Nos debemos plantear una solucion a guardar los coeficientes y exponentes de x, ya hemos resuelto el problema de los espacios y que aparezcan mas y menos al inicio del polinomio, tambien persiste el problema de guardar el stringstream y no saber si se debe usar un double o int.

--- Proyecto de programacion.cpp
+++ Proyecto de programacion.cpp
@@ -14,8 +14,10 @@ struct tPolinomio
 int main () {
 
     int n, i, j {1};
-    string equt, temp;
+    char C;
+    string equt;
     stringstream ss;
+    vector <char> vec;
     cout << "Introduzca la cantidad de polinomios a sumar: ";
     cin >> n;
     cin.ignore();
@@ -23,24 +25,53 @@ int main () {
     for (i=1; i<=n; i++)
         tPolinomio poli[i];
 
+    int m;
+
     do{
-    cout << endl << "Polinomio " << j << " :";
-    getline (cin, equt);
-    ss << equt;
-    int num;    // Toma de equt los ints
-    ss >> num;
-    cout << num;
+        cout << endl << "Polinomio " << j << " :";
+        getline (cin, equt);
+        
+        for (auto C : equt) // Copia en C cada char de equt y va analizando si es un espacio (Usamos chatGTP para conocer de isspace)
+        {
+            if (not (isspace(C)))
+            {
+                vec.push_back(C);
+            }
+        }
 
+        string rawpol(vec.data(), vec.size());  // Este nuevo string simplemente abra ignorado todos los espacios que el usuario ha insertado
+        vec.clear();
+        int p{0};
 
-    for (auto temp : equt)
+        for (auto C : rawpol)
+        {
+            p++;
+            if (not(p==1 and (C=='+')))
+            {
+                vec.push_back(C);
+            }
+        }
+
+        string pol(vec.data(), vec.size()); //Este nuevo string no contiene un mas al inicio en caso de que contenga uno
+        vec.clear();
+
+        do {  // Esta parte sigue inconclusa
+        ss << pol;
+        double num;
+        ss >> num;
+
+        int k{0};   // Lo que hace el entero es contar la posicion del ss, este va a viajar siempre entre doubles
+        k++;
+        if (k % 2 != 0)
+        {
+            coef.push_back(num);
+        }
+
+        } while () 
+
         
 
-    
-    
     j++;
-
-
-    
     } while (j<=n);
 
     return 0;
